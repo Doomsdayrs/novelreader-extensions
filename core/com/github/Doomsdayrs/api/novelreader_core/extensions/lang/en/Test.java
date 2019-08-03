11c9ade4b54e45db5764091175c8d266ba85e627
doomsdayrs <rahim13657>
2019-08-02 20:32:28 -0400

    v1.1.7 BestLightNovel

--- core/com/github/Doomsdayrs/api/novelreader_core/extensions/lang/en/Test.java
+++ core/com/github/Doomsdayrs/api/novelreader_core/extensions/lang/en/Test.java
@@ -1,22 +1,39 @@
 package com.github.Doomsdayrs.api.novelreader_core.extensions.lang.en;
 
-import com.github.Doomsdayrs.api.novelreader_core.extensions.lang.en.syosetu.Syosetu;
+import com.github.Doomsdayrs.api.novelreader_core.extensions.lang.en.bestlightnovel.BestLightNovel;
 import com.github.Doomsdayrs.api.novelreader_core.services.core.dep.Formatter;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * ====================================================================
+ */
 
 /**
- * com.github.Doomsdayrs.api.novelreader_core.extensions.lang.en
+ * novelreader-extensions
  * 03 / June / 2019
  *
  * @author github.com/doomsdayrs
  */
 class Test {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Formatter formatter = new Syosetu(1);
-        System.out.println(formatter.getNovelPassage("https://ncode.syosetu.com/n3161fj/15/"));
-        TimeUnit.SECONDS.sleep(1);
-        System.out.println(formatter.getNovelPassage("https://ncode.syosetu.com/n3161fj/1/"));
+        Formatter formatter = new BestLightNovel(1);
+        System.out.println(formatter.getNovelPassage("https://bestlightnovel.com/novel_888138300/chapter_29"));
     }
 }
--- core/com/github/Doomsdayrs/api/novelreader_core/extensions/lang/en/novel_planet/NovelPlanet.java
+++ core/com/github/Doomsdayrs/api/novelreader_core/extensions/lang/en/novel_planet/NovelPlanet.java
@@ -8,21 +8,30 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * This file is part of novelreader-extensions.
- * novelreader-extensions is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * novelreader-extensions is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with novelreader-extensions.  If not, see <https://www.gnu.org/licenses/>.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  * ====================================================================
+ */
+
+/**
  * novelreader-extensions
  * 11 / June / 2019
  *
@@ -33,18 +42,22 @@ import java.util.List;
 public class NovelPlanet extends ScrapeFormat {
     public NovelPlanet(int id) {
         super(id);
+        hasCloudFlare(true);
     }
 
     public NovelPlanet(int id, Request.Builder builder) {
         super(id, builder);
+        hasCloudFlare(true);
     }
 
     public NovelPlanet(int id, OkHttpClient client) {
         super(id, client);
+        hasCloudFlare(true);
     }
 
     public NovelPlanet(int id, Request.Builder builder, OkHttpClient client) {
         super(id, builder, client);
+        hasCloudFlare(true);
     }
 
     @Override
@@ -75,17 +88,17 @@ public class NovelPlanet extends ScrapeFormat {
 
     @Override
     public String getLatestURL(int i) {
-        return null;
+        return "";
     }
 
     @Override
     public List<Novel> parseLatest(String s) throws IOException {
-        return null;
+        return new ArrayList<>();
     }
 
     @Override
     public List<Novel> search(String s) throws IOException {
-        return null;
+        return new ArrayList<>();
     }
 
     @Override
